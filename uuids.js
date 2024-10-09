c1003a05bb7b707f4e68b085d2ff981bf5da53ea
JDubs <jmwarman@hotmail.com>
2024-10-08 21:46:40 -0600

    Block 12410000 Winners

--- uuids.js
+++ uuids.js
@@ -1,11 +1,11 @@
 const seedrandom = require("seedrandom");
 const fs = require("fs");
 
-// Hash from block #12287000
+// Hash from block #12410000
 const hash =
-  "0x6882d0f8ba2db22900fb2aaf11eddac75d478211b727cd127dfb759b518aa9d5";
+  "0x9b5b5adde42ed7175428fc4f6331219565b9e339954c9730bd3009f80d9f65cf";
 
-// isolate all numeric values from hash. ouput: '06882082229002117547821172712775951895'
+// isolate all numeric values from hash. ouput: '09554271754284633121956593399549730300980965'
 
 const seed = hash.replace(/[a-zA-Z]/g, "");
 
@@ -42,7 +42,7 @@ list
 
 // draw 90 winners
 const winnersList = [];
-for (let round = 0; round < 90; round++) {
+for (let round = 0; round < 71; round++) {
   const rnd = rng();
 
   let random = rnd * weights.reduce((a, b) => a + b, 0);
